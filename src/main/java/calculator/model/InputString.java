06dc1e5d5286736a5f28b893dce2f3161fd82115
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-10-17 13:45:46 +0900

    refactor(model): InputString 캡슐화
    - parseNumbers(String)을 private으로 감추고 getResult(String)에서 내부적으로 호출하도록 처리
    - 외부에는 문자열 한 번만 넘기면 합계를 반환하도록 처리함으로써 모델의 캡슐화를 강화

--- src/main/java/calculator/model/InputString.java
+++ src/main/java/calculator/model/InputString.java
@@ -18,20 +18,16 @@ public class InputString {
         }
 
         int endIndex = getEndIndex(source);
-        System.out.println("endIndex = " + endIndex);
-
         if (source.substring(2, endIndex - 2).length() != 1) {
             throw new IllegalArgumentException("커스텀 구분자는 문자열이 아닌 하나의 문자여야 합니다.");
         }
 
         customDelimiter = source.substring(2, endIndex - 2);
-        System.out.println(customDelimiter);  // 커스텀 구분자만 빼냄
         return customDelimiter;
     }
 
     private int getEndIndex(String source) {
         int endIndex = 0;
-
         String s = "\\n";
         if (source.contains(s)) {
             endIndex = source.indexOf(s) + 2;
@@ -40,9 +36,8 @@ public class InputString {
         return endIndex;
     }
 
-    // //?\n 이후의 문자열에 대해서만 작업해야 할 듯
-    public void parseNumbers(String source) {
-        String regex = totalSplitRegex(getCustomDelimiter(source));  // 기본 구분자 + 커스텀 구분자
+    private void parseNumbers(String source) {
+        String regex = totalSplitRegex(getCustomDelimiter(source));
         String substring = source.substring(getEndIndex(source));
 
         for (String s : substring.split(regex)) {
@@ -69,8 +64,8 @@ public class InputString {
         return list.stream().map(Pattern::quote).collect(Collectors.joining("|"));
     }
 
-    public int getResult() {
-
+    public int getResult(String source) {
+        parseNumbers(source);
         if (numberList.isEmpty()) {
             result = 0;
         }
